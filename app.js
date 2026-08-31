cd4e96f9c5e48bdf20e02f5b6e64591d59841128
Kacha Yash <ykacha243@rku.ac.in>
2026-08-31 18:03:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const apiLogger = require("./utils/logger");
 const limiter = require("./middleware/rateLimit");
 
 const app = express();
-
+console.log()
 /*
 ===========================================================
   SECURITY MIDDLEWARE
